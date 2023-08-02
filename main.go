6faf608a7a6921ef1c3b41d629d3187c6433ea4a
Christopher Aedo <Christopher.aedo.p@gmail.com>
2023-08-02 17:43:49 -0400

    siguiente commit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/ChristopherAedoP/godesde0/variables"
 )
@@ -11,4 +12,19 @@ func main() {
 
 	fmt.Println("Estado = ", estado)
 	fmt.Println("Texto = ", texto)
+
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("Esto no es windows, es ", os)
+	} else {
+		fmt.Println("Esto es windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es linux")
+	case "OS x":
+		fmt.Println("Esto es OS x")
+	default:
+		fmt.Println("%s \n", os)
+	}
 }

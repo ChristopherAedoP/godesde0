6a98814f02e2c4980dfed6fd78491ad6f8b0bbf2
Christopher Aedo <Christopher.aedo.p@gmail.com>
2023-08-02 16:52:09 -0400

    inicial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ChristopherAedoP/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ChristopherAedoP/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}

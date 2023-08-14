a333fa1d78c91e5a11af2421e315b1ba558a3e5e
Christopher Aedo <Christopher.aedo.p@gmail.com>
2023-08-14 18:43:37 -0400

    siguiente commit

--- main.go
+++ main.go
@@ -2,29 +2,33 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 
-	"github.com/ChristopherAedoP/godesde0/variables"
+	"github.com/ChristopherAedoP/godesde0/ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConvertiraTexto(8033)
+	// estado, texto := variables.ConvertiraTexto(8033)
 
-	fmt.Println("Estado = ", estado)
-	fmt.Println("Texto = ", texto)
+	// fmt.Println("Estado = ", estado)
+	// fmt.Println("Texto = ", texto)
 
-	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
-		fmt.Println("Esto no es windows, es ", os)
-	} else {
-		fmt.Println("Esto es windows")
-	}
+	// if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+	// 	fmt.Println("Esto no es windows, es ", os)
+	// } else {
+	// 	fmt.Println("Esto es windows")
+	// }
 
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Esto es linux")
-	case "OS x":
-		fmt.Println("Esto es OS x")
-	default:
-		fmt.Println("%s \n", os)
-	}
+	// switch os := runtime.GOOS; os {
+	// case "linux":
+	// 	fmt.Println("Esto es linux")
+	// case "OS x":
+	// 	fmt.Println("Esto es OS x")
+	// default:
+	// 	fmt.Println("%s \n", os)
+	// }
+
+	numero, texto := ejercicios.ConvNumerico("4")
+	fmt.Println(numero)
+	fmt.Println(texto)
+
 }
